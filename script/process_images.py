6c6cea7a8e86e22cbeb08f884ae917a2ad64e1ab
ycui <ycui@sasaki.com>
2017-07-24 15:40:12 -0400

    crop image, start/end image

--- script/process_images.py
+++ script/process_images.py
@@ -6,9 +6,10 @@ from PIL import ImageOps
 import zipfile
 import xml.etree.cElementTree as ET
 
-image_folder = sys.argv[1]  # e.g. $python imvertImages.py "C:\\Dynamo\\revit3dprint\\image\\" 2 0.0001
+image_folder = sys.argv[1]  # e.g. $python imvertImages.py "C:\\Dynamo\\revit3dprint\\image\\" 2 0.0001 6
 digits = sys.argv[2]
 voxelSize = sys.argv[3]
+border = sys.argv[4]
 
 
 def clean_up(image_folder):  # delete invert, manifest.xml
@@ -22,6 +23,27 @@ def clean_up(image_folder):  # delete invert, manifest.xml
 				shutil.rmtree(file_path)
 
 
+def summary(image_folder):
+	image_name = os.listdir(image_folder)[0]
+	image = Image.open(os.path.join(image_folder, image_name))
+	width, height = image.size  # get image dimensions
+	count = len(os.listdir(image_folder))
+	return width, height, count
+
+
+def create_blank_image(image_folder, width, height, file_name):
+	image = Image.new('RGB', (width, height), (255, 255, 255))
+	image.save(os.path.join(image_folder, file_name))
+
+
+def crop_images(image_folder, border):
+	for file_name in os.listdir(image_folder):
+		image_path = os.path.join(image_folder, file_name)
+		image = Image.open(image_path)
+		cropped_image = ImageOps.expand(image, border=border, fill='white')
+		cropped_image.save(image_path)
+
+
 def invert_images(image_folder):
 	print("start invert images...")
 	origin_image_folder = os.path.join(image_folder, "origin\\")
@@ -31,19 +53,14 @@ def invert_images(image_folder):
 	print("inverted image folder: " + invert_image_folder)
 	os.makedirs(invert_image_folder)
 
-	for filename in os.listdir(origin_image_folder):
-		if os.path.isfile(os.path.join(origin_image_folder, filename)):  # ignore folder
-			print("processing image: " + filename)
-			image = Image.open(origin_image_folder + filename)
+	for file_name in os.listdir(origin_image_folder):
+		image_path = os.path.join(origin_image_folder, file_name)
+		if os.path.isfile(image_path):  # ignore folder
+			print("processing image: " + file_name)
+			image = Image.open(origin_image_folder + file_name)
 			invert_img = image.convert("RGB")
 			inverted_image = ImageOps.invert(invert_img)
-			inverted_image.save(invert_image_folder + filename)
-
-	image_name = os.listdir(invert_image_folder)[0]
-	image = Image.open(os.path.join(invert_image_folder, image_name))
-	width, height = image.size  # get image dimensions
-	count = len(os.listdir(invert_image_folder))
-	return width, height, count
+			inverted_image.save(invert_image_folder + file_name)
 
 
 def create_xml(folder_name, digits, gridSizeX, gridSizeY, gridSizeZ, voxelSize):
@@ -71,7 +88,7 @@ def create_xml(folder_name, digits, gridSizeX, gridSizeY, gridSizeZ, voxelSize):
 def create_zip(folder_name):
 	file_name = 'model.zip'
 	zip_file = zipfile.ZipFile(os.path.join(folder_name, "model.zip"), 'w', zipfile.ZIP_DEFLATED)
-	zip_file.write("manifest.xml", compress_type=zipfile.ZIP_DEFLATED)
+	zip_file.write(os.path.join(folder_name, "manifest.xml"), "manifest.xml", compress_type=zipfile.ZIP_DEFLATED)
 	for f_name in os.listdir(os.path.join(folder_name, "invert\\")):
 		full_file_path = os.path.join(folder_name, "invert\\" + f_name)
 		relative_file_path = os.path.relpath(full_file_path, folder_name)
@@ -79,9 +96,19 @@ def create_zip(folder_name):
 	zip_file.close()
 	print("created zip file: " + folder_name + file_name)
 
-	
+
 clean_up(image_folder)
-width, height, count = invert_images(image_folder)
+
+origin_image_folder = os.path.join(image_folder, "origin")
+width, height, count = summary(origin_image_folder)
+
+start_image_name = str(0).rjust(int(digits), '0') + ".png"
+end_image_name = str(count + 1).rjust(int(digits), '0') + ".png"
+create_blank_image(origin_image_folder, width, height, start_image_name)
+create_blank_image(origin_image_folder, width, height, end_image_name)
+
+crop_images(origin_image_folder, int(border))
+invert_images(image_folder)
 create_xml(image_folder, digits, width, height, count, voxelSize)  # todo: match x, y, z order
 create_zip(image_folder)
 print("done")
